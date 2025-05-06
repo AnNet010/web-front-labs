8640d82f8c3a3f955d3fd54cbc7ce616bfad27c8
Анна Боброва <annabobrova05@yandex.ru>
2025-05-06 17:16:06 +0700

    Список книг

---
+++ lab6/books.js
@@ -0,0 +1,83 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 414,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Харпер Ли',
+        name: 'Убить пересмешника',
+        publicationYear: 2023,
+        price: 423,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Теодор Драйзер',
+        name: 'Финансист',
+        publicationYear: 2022,
+        price: 220,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Данте Алигьери',
+        name: 'Божественная комедия',
+        publicationYear: 2024,
+        price: 223,
+        bestseller: true,
+        inStock: true
+    },
+
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 2023,
+        price: 251,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2023,
+        price: 300,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Александр Грибоедов',
+        name: 'Горе от ума',
+        publicationYear: 2023,
+        price: 166,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 2024,
+        price: 223,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Борис Полевой',
+        name: 'Повесть о настоящем человеке',
+        publicationYear: 2022,
+        price: 311,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2025,
+        price: 361,
+        bestseller: true,
+        inStock: true
+    }
+];
